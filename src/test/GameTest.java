308bc49b20b613666041a4db8e575d6461e53895
Sasha <al.kanenckoff@yandex.ru>
2021-10-11 20:42:42 +0300

    add tests to check the correct completion of the game

--- src/test/GameTest.java
+++ src/test/GameTest.java
@@ -41,4 +41,33 @@ class GameTest {
         Game.initQuestions();
         assertFalse(Game.checkAnswer(Game.getQuestions().get(1), 2, 1));
     }
+
+    @Test
+    public void checkRightAnswerAndGetMoneyTest() {
+        InputStream inputStream = System.in;
+        ByteArrayInputStream in = new ByteArrayInputStream("0".getBytes());
+        System.setIn(in);
+        Game.initQuestions();
+        assertFalse(Game.checkAnswer(Game.getQuestions().get(1), 3, 1));
+        System.setIn(inputStream);
+        Game.getQuestions().clear();
+    }
+
+    @Test
+    public void checkRightAnswerAndContinueTest() {
+        InputStream inputStream = System.in;
+        ByteArrayInputStream in = new ByteArrayInputStream("1".getBytes());
+        System.setIn(in);
+        Game.initQuestions();
+        assertTrue(Game.checkAnswer(Game.getQuestions().get(1), 3, 1));
+        System.setIn(inputStream);
+        Game.getQuestions().clear();
+    }
+
+    @Test
+    public void checkRightAnswerEndGameTest() {
+        Game.initQuestions();
+        assertFalse(Game.checkAnswer(Game.getQuestions().get(4), 3, 4));
+        Game.getQuestions().clear();
+    }
 }
